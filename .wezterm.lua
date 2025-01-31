fd6e313119a3e83c591cf7ef0d36614c44f173dc
Robbware <robb.alves93@gmail.com>
2025-01-31 18:15:49 +0000

    Initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,87 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	color_scheme = "Afterglow",
+	default_prog = { "pwsh" },
+	default_cwd = "~",
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	default_cursor_style = "BlinkingBar",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 11,
+}
+
+config.keys = {
+	{
+		key = "h",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "v",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "A", mods = "CTRL" }),
+	},
+}
+
+config.colors = {
+	tab_bar = {
+		background = "#0b0022",
+		active_tab = {
+			bg_color = "#2b2042",
+			fg_color = "#c0c0c0",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+		},
+		inactive_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+		},
+		new_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+		},
+	},
+}
+
+local launch_menu = {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "C:/Program Files/PowerShell/7/pwsh.exe", "-NoLogo" },
+	})
+
+	-- Find installed visual studio version(s) and add their compilation
+	-- environment command prompts to the menu
+	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+		local year = vsvers:gsub("Microsoft Visual Studio/", "")
+		table.insert(launch_menu, {
+			label = "x64 Native Tools VS " .. year,
+			args = {
+				"cmd.exe",
+				"/k",
+				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+			},
+		})
+	end
+end
+
+config.launch_menu = launch_menu
+
+return config
